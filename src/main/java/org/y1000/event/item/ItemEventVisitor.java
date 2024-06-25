1dbee06ffd9316732cd0c2a7cd678beb62ee2695
Kyle Lin <kyle.lin@wirelesscar.com>
2024-06-25 17:59:58 +0800

    commit.

--- src/main/java/org/y1000/event/item/ItemEventVisitor.java
+++ src/main/java/org/y1000/event/item/ItemEventVisitor.java
@@ -1,14 +1,9 @@
 package org.y1000.event.item;
 
-import org.y1000.event.EntityEvent;
 import org.y1000.event.EntityEventVisitor;
 import org.y1000.message.PlayerDropItemEvent;
-import org.y1000.message.serverevent.ShowItemEvent;
 
 public interface ItemEventVisitor extends EntityEventVisitor {
-    default void visit(ShowItemEvent event) {
-        visit((EntityEvent) event);
-    }
     default void visit(PlayerDropItemEvent dropItemEvent) {
 
     }
