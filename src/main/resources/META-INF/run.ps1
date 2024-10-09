6391011962866a0e54402f84c8d157f13dddf8e7
Kyle <linbaiye@gmail.com>
2024-10-09 22:32:44 +0800

    Fix deploy.

--- src/main/resources/META-INF/run.ps1
+++ src/main/resources/META-INF/run.ps1
@@ -4,8 +4,8 @@ If(!(test-path -PathType container $path))
     New-Item -ItemType Directory -Path $path
 }
 Remove-Item -Force -Recurse "$path\*"
-Copy-Item "\\tsclient\D\work\java\1000server\target\Server-1.0-SNAPSHOT-jar-with-dependencies.jar" $path\Server.zip
-Set-Location $path
-Expand-Archive .\Server.zip .\
+Add-Type -Assembly "System.IO.Compression.Filesystem"
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$path\Server.zip", "$path")
 Copy-Item -Force .\META-INF\prod.xml .\META-INF\persistence.xml
 Copy-Item -Force logprod.xml logback.xml
+java -Xmx800m -Xms500m -cp . org.y1000.Server
