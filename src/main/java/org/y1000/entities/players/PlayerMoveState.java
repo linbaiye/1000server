a43296af937c66b3f6e47ced71bbe3607f4825db
Kyle <linbaiye@gmail.com>
2025-07-01 00:19:19 +0800

    a few fixes.

--- src/main/java/org/y1000/entities/players/PlayerMoveState.java
+++ src/main/java/org/y1000/entities/players/PlayerMoveState.java
@@ -78,6 +78,7 @@ public final class PlayerMoveState extends AbstractPlayerState {
     @Override
     public void move(MoveInput moveInput) {
         newInput = moveInput;
+
     }
 
     @Override
