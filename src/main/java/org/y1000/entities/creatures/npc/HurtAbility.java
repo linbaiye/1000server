b40f13c0d2014a144893e9d6965a93cfa503c75e
Kyle <linbaiye@gmail.com>
2025-07-03 08:22:42 +0800

    commit.

--- src/main/java/org/y1000/entities/creatures/npc/HurtAbility.java
+++ src/main/java/org/y1000/entities/creatures/npc/HurtAbility.java
@@ -1,17 +1,54 @@
 package org.y1000.entities.creatures.npc;
 
 import org.y1000.entities.AttackableEntity;
+import org.y1000.entities.players.Damage;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class HurtAbility {
 
     private final Npc npc;
 
-    public HurtAbility(Npc npc) {
+    private final int armor;
+
+    private final int avoidance;
+
+    private final int hurtSound;
+
+    private final int millis;
+
+    private int life;
+
+
+    public HurtAbility(Npc npc,
+                       int armor,
+                       int avoidance,
+                       int hurtSound,
+                       int life,
+                       int millis) {
         this.npc = npc;
+        this.armor = armor;
+        this.avoidance = avoidance;
+        this.hurtSound = hurtSound;
+        this.millis = millis;
+        this.life = life;
+    }
+    protected boolean isDodged(int attackerHit) {
+        var rand = ThreadLocalRandom.current().nextInt(0, attackerHit + 75 + avoidance);
+        return rand < avoidance;
     }
 
-    public void attackedBy(AttackableEntity attacker) {
-        npc.changeState(new NpcHurtState());
+    public void attackedBy(AttackableEntity attacker, Damage damage, int hit) {
+        if (isDodged(hit))
+            return;
+        var before = life;
+        damageAction.accept(damage);
+        var damagedLife = before - currentLife();
+        if (damagedLife > 0) {
+            var exp = damagedLifeToExp(damagedLife);
+            gainExp.accept(exp);
+        }
+        npc.changeState();
         npc.findAbility(AttackAbility.class).ifPresent(attackAbility -> attackAbility.setEnemy(attacker));
     }
 }
